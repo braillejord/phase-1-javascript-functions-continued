fb6793d47378d9962457823e0fd19fe5203d1344
BreElle Wells <breellejordyn@gmail.com>
2023-07-11 07:37:51 -0600

    complete functions: continued lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity) {
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`;
+    } else {
+        return "This Saturday, I want to roller-skate!";
+    }
+}
+
+function mondayWork(activity) {
+    if (activity) {
+        return `This Monday, I will ${activity}.`;
+    } else {
+        return "This Monday, I will go to the office.";
+    }
+}
+
+function wrapAdjective(style='*') {
+    return function (adj = 'special') {
+        return `You are ${style}${adj}${style}!`;
+    }
+}
